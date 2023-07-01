8d0170680fd0165e630576ba5f1a3963e5a2f255
mustomi <mustomi07@gmail.com>
2023-07-01 23:50:31 +0700

    tambah fitur halaman popup

--- js/script.js
+++ js/script.js
@@ -30,6 +30,25 @@ const Malam = () => {
     modeMalam.style.display = "none"
 }
 
+const popup = document.querySelector('.popup');
+const x = document.querySelector('.popup-content h1')
+
+window.addEventListener('load', () => {
+    popup.classList.add('showPopup');
+    popup.childNodes[1].classList.add('showPopup');
+    })
+
+x.addEventListener('click', () => {
+    popup.classList.remove('showPopup');
+    popup.childNodes[1].classList.remove('showPopup');
+    })
+
+const BtnNext = () => {
+    popup.classList.remove('showPopup');
+    popup.childNodes[1].classList.remove('showPopup');
+    window.location = "index.html#halaman-pengumuman";
+}
+
 const listArsipJuli = document.getElementById("list-arsip-juli")
 const listButtonJuli = document.getElementById("list-button-juli")
 const listButtonJuliTutup = document.getElementById("list-button-juli-tutup")
